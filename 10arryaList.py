7340d57af85fabab1342f29c6369d80d5f94736a
Zohirul Haque <zhremon@outlook.com>
2023-09-20 19:54:21 +0600

    Third commit

---
+++ 10arryaList.py
@@ -0,0 +1,7 @@
+#array list
+#index      0       1         2
+fruits = ["Mango", "Apple", "Orange"]
+#print(fruits[1])
+#using loop
+for fruit in fruits: #fruits = Mango
+  print(fruit)
---
+++ 11functionMethod.py
@@ -0,0 +1,10 @@
+#function/method
+#def funName():
+def addTwoNum(a, b):
+  return a+b
+
+def subTwoNum(a,b):
+  return a - b
+
+result = subTwoNum(24000, 17000)
+print(result)
