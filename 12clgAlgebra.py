e51a14a591f647eb206b41a4892873e7dd5f32fe
Zohirul Haque <zhremon@outlook.com>
2023-10-03 00:22:45 +0600

    fourth commit

---
+++ 12clgAlgebra.py
@@ -0,0 +1,16 @@
+#Set up a proportion:
+#n1/d1 = n2/d2
+
+# Put a zero in for the unknown value
+n1 = 1
+d1 = 2
+n2 = 0
+d2 = 16
+
+if n2==0:
+      answer = d2 * n1 / d1
+      print("n2 = ", answer)
+
+if d2==0:
+      answer = n2 * d1 / n1
+      print("d2 = ", answer)
