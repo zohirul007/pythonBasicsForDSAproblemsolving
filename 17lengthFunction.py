e173328edf2c5baba19f17c7312f53e50d1949ff
Zohirul Haque <zhremon@outlook.com>
2023-10-25 08:17:41 +0600

    len function to get the index of a string

--- 17lengthFunction.py
+++ 17lengthFunction.py
@@ -3,3 +3,9 @@ name = "Tanjin"
 
 print(len(name))  # len function is using to find length in python string
 print(name[0])
+print(name[-1])
+print(name[0:3])
+print(name[0:])
+print(name[:3])  # by default python  put 0 index on first
+print(name[:])  # if we don't declare the start and the end of
+# the string then python will give return the original copy of the string
