319ff1bae7faf7656da5736cf661dc8dfef91582
Zohirul Haque <zhremon@outlook.com>
2023-10-30 11:21:30 +0600

    escape sequences in python

---
+++ 18escapeSequencePython.py
@@ -0,0 +1,9 @@
+#if we are want to show a " " " double quotation in a " String " we can use
+# \"
+# \'
+# \\ to show a single \ in a line
+# \n for new line
+
+name = "Tanjin \nTisha"
+
+print(name)
