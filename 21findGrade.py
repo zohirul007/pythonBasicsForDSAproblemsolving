1bf64bdd387ea5a03acbe6b3fbf372f94a1e48ff
Zohirul Haque <zhremon@outlook.com>
2023-11-08 11:57:56 +0600

    Find grades with if condition

---
+++ 21findGrade.py
@@ -0,0 +1,9 @@
+# If the student receives over 40% on her test, report that her grade passes; 
+# if not, report that her grade fails
+
+grade = 39
+
+if grade >= 40:
+    print("Your grade is: Pass")
+else:
+    print("Your grade is: Fail")
