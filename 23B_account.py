9ad9abcb30e1baf267c9eb1d9e8823ac0db42f87
Zohirul Haque <zhremon@outlook.com>
2023-11-19 14:24:23 +0600

    conditionals

---
+++ 23B_account.py
@@ -0,0 +1,11 @@
+#f he has money in his account, calculate interest; 
+#if he doesn’t, charge a penalty fee.
+# when if statement failed then use if else statement
+
+balance = 801
+ 
+if balance < 0:
+    print("Balance is below 0, add funds now or you will be charged a penalty.")
+ 
+else:
+    print("Your balance is 0 or above.")
