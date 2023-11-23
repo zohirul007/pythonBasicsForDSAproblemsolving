19c51a9bfe91ece5cf7f0d55ca8b710915389089
Zohirul Haque <zhremon@outlook.com>
2023-11-23 19:14:23 +0600

    conditionals in python

--- 21findGrade.py
+++
@@ -1,9 +0,0 @@
-# If the student receives over 40% on her test, report that her grade passes; 
-# if not, report that her grade fails
-# with if statement
-grade = 39
-
-if grade >= 40:
-    print("Your grade is: Pass")
-else:
-    print("Your grade is: Fail")
